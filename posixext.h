618eff3a50b4dc0eb8766965882e6b079e7e904b
Freya of FractalSystem <us@fractalsys.net>
2024-12-02 00:54:56 -0800

    moved timegm into the conditional extern c block to allow C++ code to access it

--- posixext.h
+++ posixext.h
@@ -5,10 +5,10 @@
 #include <stdlib.h>
 #include <time.h>
 
-    time_t timegm(struct tm *tm);
 #ifdef __cplusplus
 extern "C" {
 #endif
+    time_t timegm(struct tm *tm);
 
 FILE *fmemopen(void *buf, size_t size, const char *mode);
 FILE *open_memstream(char **bufptr, size_t *sizeptr);
